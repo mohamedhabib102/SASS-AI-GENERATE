4f67d0eb5ca5663fa476ed16c36b3ef135074a83
Mohamed Habib <byaalkhy43@gmail.com>
2026-06-15 12:18:17 +0300

    Updated: editing complete 'CustomInput' and handling show/hide

--- src/components/shared/CustomInput.jsx
+++ src/components/shared/CustomInput.jsx
@@ -1,4 +1,4 @@
-import { Input } from "../ui/input";
+import { useState } from "react";
 
 
 
@@ -12,9 +12,19 @@ const CustomInput = ({
     className,
     classLabel,
     icon,
-    iconPassword
+    iconEyeClosed,
+    iconsEyeDashed
 }) => {
-    const Icon = icon
+    const IconBase = icon;
+    const IconShow = iconEyeClosed;
+    const IconHide = iconsEyeDashed;
+
+    const [password, setPassword] = useState(false);
+
+
+    const handlingShowPassowrd = () => {
+        setPassword(!password)
+    }
     
     return (
     <div className="w-full mb-4 relative">
@@ -24,14 +34,23 @@ const CustomInput = ({
 
 
      <input
-      type={type}
+      type={password ? "password" : "text"}
       name={name}
       placeholder={palceholder}
       id={id}
       className={className}
       />
-     {icon && <span className="absolute right-2 text-[#9E9E9E] top-1/2 -translate-y-1/2">{<Icon size={20} />}</span>}
+
+
+     {icon && <span className="absolute right-2 text-[#9E9E9E] top-1/2 -translate-y-1/2">{<IconBase size={18} />}</span>}
+     {type === "password" && <button 
+       onClick={handlingShowPassowrd}
+     className="cursor-pointer absolute left-2 text-[#9E9E9E] top-1/2 -translate-y-1/2">{
+        password ? <IconShow size={18} /> : <IconHide size={18} />
+     }</button>}
       </div>
+
+
 
     </div>
     )
--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,20 +1,28 @@
 import LayoutFroms from "@/components/layouts/LayoutFroms";
 import CustomInput from "@/components/shared/CustomInput";
-import { User } from "lucide-react";
+import { Eye, EyeClosed, Lock, User } from "lucide-react";
 
 const SignIn = () => {
+
+    const handleChang = (e) => {
+        e.preventDefault()
+    }
   return (
     <section className="min-h-screen">
       <LayoutFroms title={"Sign In"} description={"Tester"}>
-        <form>
+        <form
+        onSubmit={handleChang}
+        >
           <CustomInput
             name={"fullName"}
-            type={"text"}
+            type={"password"}
             classLabel="block mb-1 text-[#121212]"
             id={"fName"}
             labelContent={"الأسم كامل"}
             palceholder={"محمد موافي"}
             icon={User}
+            iconEyeClosed={EyeClosed}
+            iconsEyeDashed={Eye}
             className={"pr-8 w-full tetx-sm text-[#9E9E9E] block outline-none border-[#E5E5E5] border rounded-lg px-4 py-2"}
           />
         </form>
