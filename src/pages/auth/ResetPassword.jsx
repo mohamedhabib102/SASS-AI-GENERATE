1ec51b09b3ba394c6dc1d67297abf82289e9c0a9
yousef yahia <yoousefyahia@gmail.com>
2026-06-16 14:48:23 +0300

    Add Reset Password feature

--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -1,27 +1,30 @@
 import LayoutForms from "@/components/layouts/LayoutForms";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { Mail } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
 import CustomInput from "@/components/shared/CustomInput";
 import { Button } from "@/components/ui/button";
+import { useTranslation } from "react-i18next";
+import { Eye, EyeClosed, Lock } from "lucide-react";
 
 export default function ResetPassword() {
+        const { t, i18n } = useTranslation();
+        const lang = i18n.language || 'ar';
 
-  const validationSchema = Yup.object({
-    email: Yup.string()
-      .email("البريد الإلكتروني غير صالح")
-      .required("البريد الإلكتروني مطلوب"),
-  });
+    const validationSchema = Yup.object({
+        password: Yup.string().min(6, lang === 'en' ? 'Password must be at least 6 characters' : 'كلمة المرور مطلوبه').required(lang === 'en' ? 'Password is required' : 'كلمة المرور مطلوبة'),
+        'terms-conditions': Yup.boolean().oneOf([true], lang === 'en' ? 'You must agree to the terms and conditions' : 'يجب الموافقة على الشروط والأحكام')
+    });
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      "terms-conditions": false,
-    },
-    validationSchema,
-    onSubmit: (values) => {
-    },
-  });
+    const formik = useFormik({
+        initialValues: {
+            password: '',
+            'terms-conditions': false,
+        },
+        validationSchema,
+        onSubmit: (values) => {
+        }
+    });
 
   return (
     <LayoutForms
@@ -32,28 +35,71 @@ export default function ResetPassword() {
           className="w-100 h-16 mb-10"
         />
       }
-      title="نسيت كلمة المرور؟"
-      description="أدخل بريدك الالكتروني الذي تستخدمه لاختيار كلمة مرور جديدة."
+      title="إعادة تعيين كلمة المرور"
+      description="أدخل كلمة مرور قوية لحماية حسابك"
     >
 
                 <form onSubmit={formik.handleSubmit} className="w-full lg:w-[80%] mx-auto">
                             <div className="form-fields p-4 flex flex-col gap-4 my-4" >
-        <CustomInput
-          name="email"
-          type="email"
-          id="email"
-          labelContent="البريد الإلكتروني"
-          palceholder="أدخل بريدك الإلكتروني"
-          icon={Mail}
-          formik={formik}
-        />
+<CustomInput
+                            name="password"
+                            type="password"
+                            id="password"
+                            labelContent={lang === 'en' ? "Password" : "أدخل كلمة مرور جديدة"}
+                            palceholder={lang === 'en' ? "Enter your password" : "********"}
+                            icon={Lock}
+                            iconEyeClosed={EyeClosed}
+                            iconsEyeDashed={Eye}
+                            formik={formik}
+                            lang={lang}
+                        />
+                        <CustomInput
+                            name="password"
+                            type="password"
+                            id="password"
+                            labelContent={lang === 'en' ? "Password" : "تأكيد كلمة المرور"}
+                            palceholder={lang === 'en' ? "Enter your password" : "********"}
+                            icon={Lock}
+                            iconEyeClosed={EyeClosed}
+                            iconsEyeDashed={Eye}
+                            formik={formik}
+                            lang={lang}
+                        />
        </div>
+       <div className={`terms flex flex-col  w-full `}>
+                            <div className={`flex items-center gap-2 `}>
+                                <Checkbox
+                                    id="terms-conditions"
+                                    name="terms-conditions"
+                                    checked={formik.values['terms-conditions']}
+                                    onCheckedChange={(checked) => formik.setFieldValue('terms-conditions', !!checked)}
+                                    onBlur={formik.handleBlur}
+                                    className=" cursor-pointer text-white bg-main w-4 h-4 border-gray-400"
+                                />
+                                <label
+                                    htmlFor="terms-conditions"
+                                    className="text-gray-600 text-sm ms-2 select-none cursor-pointer"
+                                >
+                                    {lang === 'en' ? (
+                                        <>
+                                            I agree to the <span className="font-bold text-primary text-sm lg:text-lg">Terms and Conditions and Privacy Policy</span>
+                                        </>
+                                    ) : (
+                                        <>
+                                            أوافق على <span className="font-bold text-primary text-sm lg:text-lg"> الشروط والاحكام وسياسة الخصوصية</span>
+                                        </>
+                                    )}
+                                </label>
+                            </div>
+                            {formik.touched['terms-conditions'] && formik.errors['terms-conditions'] ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors['terms-conditions']}</div>
+                            ) : null}
+                        </div>
         <Button
           type="submit"
           className="w-full mt-4 py-6 text-white font-semibold text-md cursor-pointer"
         >
-          ارسال رمز التحقق
-        </Button>
+تعيين كلمة المرور        </Button>
       </form>
 
     </LayoutForms>
