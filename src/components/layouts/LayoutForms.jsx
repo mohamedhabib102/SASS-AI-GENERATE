c521d7acded335c9ea1af9016d6db924b0ccc7d9
Ahmed Elnemr <elnemr21092003@gmail.com>
2026-06-17 15:17:32 +0300

    edit layoutForm

--- src/components/layouts/LayoutForms.jsx
+++ src/components/layouts/LayoutForms.jsx
@@ -12,7 +12,7 @@ const LayoutForms = ({ children, title, description, srcImg }) => {
       {/* left side */}
       <div className="md:w-1/2 w-full flex items-center justify-center px-6 lg:py-0 py-4">
         <div className="w-full max-w-xl">
-         <div className="flex flex-col items-center justify-center gap-5 mb-5 mt-6">
+          <div className="flex flex-col items-center justify-center gap-5 mb-5 mt-6">
               {srcImg && (
               <div>
                 <img
