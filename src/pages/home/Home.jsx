e5dcb02318d99832104ac1ae5bae76fcc89c9510
Mohamed Habib <byaalkhy43@gmail.com>
2026-06-17 14:25:39 +0300

    solve confilct in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-// import ButtonSwitchLang from "@/components/shared/ButtonSwitchLang";
-=======
 import Header from "@/components/layouts/Header";
 import ButtonSwitchLang from "@/components/shared/ButtonSwitchLang";
->>>>>>> 5dfe4d8050c1e3436d07794626c11d176da1e49d
 import CustomContainer from "@/components/shared/CustomContainer";
-import ButtonSwitchLang from "@/components/shared/ButtonSwitchLang";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
-<<<<<<< HEAD
-  const { t } = useTranslation();
-  return (
-    <section>
-      <CustomContainer>
-        Home Page
-        <ButtonSwitchLang />
-        <p>{t("home.nav.home")}</p>
-      </CustomContainer>
-    </section>
-  );
-};
-export default Home;
-=======
     const {t} = useTranslation()
     return (
         <section>
@@ -36,4 +17,3 @@ export default Home;
         </section>
     )
 }; export default Home;
->>>>>>> 5dfe4d8050c1e3436d07794626c11d176da1e49d
