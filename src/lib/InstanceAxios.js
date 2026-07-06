691e4aa7ecdba214ffbde53df2d17eefc127d9a6
yousef yahia <yoousefyahia@gmail.com>
2026-07-06 23:52:18 +0300

    add middleware  & edit translate name of errors

--- src/lib/InstanceAxios.js
+++ src/lib/InstanceAxios.js
@@ -1,10 +1,36 @@
 import axios from "axios";
 
-
-
 export const instanceAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials:  false
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: false,
+  timeout: 10000,
+});
 
+instanceAxios.interceptors.response.use(
+  (response) => response,
 
+  (error) => {
+    if (!error.response) {
+      return Promise.reject({
+        ...error,
+        messageKey: "errors.noInternet",
+      });
+    }
+
+    const status = error.response.status;
+
+    const statusMap = {
+      403: "errors.forbidden",
+      429: "errors.tooManyRequests",
+      500: "errors.serverError",
+      502: "errors.serverError",
+      503: "errors.serverError",
+      504: "errors.serverError",
+    };
+
+    return Promise.reject({
+      ...error,
+      messageKey: statusMap[status] || "errors.somethingWentWrong",
+    });
+  }
+);
